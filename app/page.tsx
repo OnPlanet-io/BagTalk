a681d2ea57facc0e6e7ab8caba7204c01aa16b2f
Ali Razzaq <alirazzaquet@gmail.com>
2025-07-22 23:08:21 -0400

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <>
       <Head>
         <title>BagTalk — Coming Soon</title>
-
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         {/* Google Fonts */}
